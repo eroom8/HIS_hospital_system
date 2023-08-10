ee0c1cc93d8b9abacf98321e09f91b18c55ec45d
eroom8 <eroom1998@gmail.com>
2023-08-10 19:36:40 +0300

    Add initial code

--- HIS_patient_input_form_withDB/connect.php
+++ HIS_patient_input_form_withDB/connect.php
@@ -1,46 +1,46 @@
 <?php
-    $telephone = $_POST['telephone'];
-    $name = $_POST['name'];
-    $dob = $_POST['dob'];
-    $idNumber = $_POST['idNumber'];
-    $address = $_POST['address'];
-    $county = $_POST['county'];
-    $subCounty = $_POST['subCounty'];
-    $patientEmail = $_POST['patientEmail'];
-    $gender = $_POST['gender'];
-    $maritalStatus = $_POST['maritalStatus'];
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hospital"; // Change this to "test"
 
-    $kinName = $_POST['kinName'];
-    $kinDob = $_POST['kinDob'];
-    $kinIdNumber = $_POST['kinIdNumber'];
-    $kinGender = $_POST['kinGender'];
-    $relationship = $_POST['relationship'];
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Database connection
-    $conn = new mysqli('localhost', 'root', '', 'test');
-    if ($conn->connect_error) {
-        echo "$conn->connect_error";
-        die("Connection Failed: " . $conn->connect_error);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Process the form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $telephone = $_POST["telephone"];
+    $name = $_POST["name"];
+    $dob = $_POST["dob"];
+    $idNumber = $_POST["idNumber"];
+    $address = $_POST["address"];
+    $county = $_POST["county"];
+    $subCounty = $_POST["subCounty"];
+    $patientEmail = $_POST["patientEmail"];
+    $gender = $_POST["gender"];
+    $maritalStatus = $_POST["maritalStatus"];
+    $kinName = $_POST["kinName"];
+    $kinDob = $_POST["kinDob"];
+    $kinIdNumber = $_POST["kinIdNumber"];
+    $kinGender = $_POST["kinGender"];
+    $relationship = $_POST["relationship"];
+
+    // Prepare and execute SQL query
+    $sql = "INSERT INTO register (telephone, name, dob, idNumber, address, county, subCounty, patientEmail, gender, maritalStatus, kinName, kinDob, kinIdNumber, kinGender, relationship)
+            VALUES ('$telephone', '$name', '$dob', '$idNumber', '$address', '$county', '$subCounty', '$patientEmail', '$gender', '$maritalStatus', '$kinName', '$kinDob', '$kinIdNumber', '$kinGender', '$relationship')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
     } else {
-        // Generate a random patient reference number (you can use any method suitable for your system)
-        $patientRefNumber = uniqid('PREF', true);
-
-        $stmt = $conn->prepare("INSERT INTO registration (firstName, lastName, gender, email, password, number, telephone, dob, idNumber, address, county, subCounty, patientEmail, maritalStatus, kinName, kinDob, kinIdNumber, kinGender, relationship, patientRefNumber) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssssissssssssssssss", $name, $dob, $gender, $patientEmail, $password, $number, $telephone, $dob, $idNumber, $address, $county, $subCounty, $patientEmail, $maritalStatus, $kinName, $kinDob, $kinIdNumber, $kinGender, $relationship, $patientRefNumber);
-        $execval = $stmt->execute();
-        if ($execval) {
-            // Send an email to the patient with the patient reference number
-            $to = $patientEmail;
-            $subject = "Patient Registration - Reference Number";
-            $message = "Dear " . $name . ",\n\nThank you for registering as a patient. Your patient reference number is: " . $patientRefNumber . ". Please keep this number for future reference.\n\nBest regards,\nYour Healthcare Provider";
-            $headers = "From: your-email@example.com"; // Replace with the actual email address
-            mail($to, $subject, $message, $headers);
-
-            echo "Registration successfully. An email with the patient reference number has been sent to the patient.";
-        } else {
-            echo "Error: " . $stmt->error;
-        }
-        $stmt->close();
-        $conn->close();
+        echo "Error: " . $sql . "<br>" . $conn->error;
     }
+}
+
+$conn->close();
 ?>
